f72938a42caa82e6ce6a28ad73a177338ae20179
Jakub Kozik <jakubkozik120@gmail.com>
2022-08-26 19:25:41 +0200

    Fix server

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,11 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/products', productsRouters);
-app.use('/api/users', usersRouters);
+import productsRoutes from './routes/products.routes';
+import usersRoutes from './routes/users.routes';
+
+app.use('/api/products', productsRoutes);
+app.use('/api/users', usersRoutes);
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
